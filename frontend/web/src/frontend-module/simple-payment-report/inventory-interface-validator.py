02c6c342209d2976c24374c04612c4c1ad36285a
aditikarbasov <aditikarbasov@users.noreply.github.com>
2025-11-16 10:17:56 +0000

    Add resolver.yml, hooks-common.json, inventory-interface-validator.py

---
+++ frontend/web/src/frontend-module/simple-payment-report/inventory-interface-validator.py
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/smarthome_fhe/frontend/web/src/frontend-module/simple-payment-report/inventory-interface-validator.py
+// Generated at: 2025-12-03T23:45:47.943Z
+
+
